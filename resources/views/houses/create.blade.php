57c67cf171a10b19d7cf4df9608b556a9af1357f
Albert de Haro <Albert.deharo.murcia@gmail.com>
2019-05-16 22:26:18 +0200

    Initial commit

---
+++ resources/views/houses/create.blade.php
@@ -0,0 +1,20 @@
+
+@extends('layouts.app')
+
+@section('title' , 'Houses Create')
+
+@section('content')
+
+    <div class="container">
+    <form class="form-group" method="POST" action="/houses">
+        @csrf
+        <div class="form-group">
+            <label for="">Nombre</label>
+            <input type="text" name="nombre" class="form-control">
+        </div>
+            <button type="submit" class="btn btn-primary">Guardar</button>
+    </form>
+    </div>
+@endsection 
+
+
